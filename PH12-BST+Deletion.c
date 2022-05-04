c475b9c26c1d53ab01d25654f4171c57b4d2c8d2
cheema-86 <ysc1571013@gmail.com>
2022-05-04 12:28:52 +0530

    inserted working code from geeksforgeeks

--- PH12-BST+Deletion.c
+++ PH12-BST+Deletion.c
@@ -36,6 +36,46 @@ struct node * insert(struct node* current, int item){
 
 }
 
+struct node* min(struct node* node){
+    struct node* current = node;
+
+    while (current && current->left != NULL)
+        current = current->left;
+ 
+    return current;
+}
+ 
+ 
+struct node* deleteNode(struct node* root, int item){
+    if (root == NULL)
+        return root;
+ 
+    if (item < root->data){
+        root->left = deleteNode(root->left, item);
+    } else if (item > root->data){
+        root->right = deleteNode(root->right, item);
+    } else {
+
+        if (root->left == NULL) {
+            struct node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            struct node* temp = root->left;
+            free(root);
+            return temp;
+        }
+ 
+        struct node* temp = min(root->right);
+
+        root->data = temp->data;
+
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
 /*
 struct node * search(struct node* current, int item){
     struct node * newnode;
